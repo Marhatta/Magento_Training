d8f8774b60a6a38ca29759b38191b4793700bdea
Marhatta <vishal.marhatta@gmail.com>
2021-01-27 15:42:17 +0530

    Updated admin grid block

--- app/code/Learning/OrmDb/Block/Adminhtml/CustomerRequests.php
+++ app/code/Learning/OrmDb/Block/Adminhtml/CustomerRequests.php
@@ -6,9 +6,6 @@ class CustomerRequests extends \Magento\Backend\Block\Widget\Grid\Container
 
 	protected function _construct()
 	{
-		$this->_controller = 'adminhtml_customerrequests';
-		$this->_blockGroup = 'Learning_OrmDb';
-		$this->_headerText = __('Customer Requests');
 		parent::_construct();
 		// must be placed after the parent::__construct();
         $this->buttonList->remove('add');
